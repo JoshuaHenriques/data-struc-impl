2925482696f67880b6bb79a87c4c10a77f12cf6a
joshuahenriques <joshuahenriques95@gmail.com>
2021-09-22 23:43:33 -0400

    Update HashMap.java

--- hash/HashMapImpl.java
+++ hash/HashMapImpl.java
@@ -1,3 +1,28 @@
-public class HashMapImpl {
+public class HashMapImpl<T> {
+	public static class Node<T> {
+		private T data;
+		private Node<T> next;
+
+		public Node() {
+			this.data = null;
+			this.next = null;
+		}
+
+		public Node(T data) {
+			this.data = data;
+		}
+	}
+
+	private Node<T>[] buckets;
+	private int size;
+
+	public HashMapImpl() {
+		this.size = 8;
+		this.buckets = new Node<T>[size];
+	}
 	
+	public HashMapImpl(int size) {
+		this.size = size;
+		this.buckets = new Node<T>[this.size];
+	}
 }
