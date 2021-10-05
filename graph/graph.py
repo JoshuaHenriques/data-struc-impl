439cfbbbff4ecf67e34adbf6925413dfeb26dbdd
joshuahenriques <joshuahenriques95@gmail.com>
2021-10-05 05:36:09 -0400

    update graph.py

--- graph/graph.py
+++ graph/graph.py
@@ -1,25 +1,12 @@
-import numpy as np
-
-if __name__ == "__main__":
-	vertices = {0, 1, 2, 3, 4, 5}
-	edges = {(0, 1), (1, 2), (0, 3), (1, 3), (3, 4), (2, 5), (4, 5), (2, 4)}
-	## adjacencyMatrix = [[0]*len(vertices)]*len(vertices)
-	adjacencyMatrix = np.zeros((len(vertices), len(vertices))).astype(int)
-	adjacencyList = {}
-	for vertex in vertices:
-		adjacencyList[vertex] = []
-	for edge in edges:
-		v1 = edge[0]
-		v2 = edge[1]
-		adjacencyMatrix[v1][v2] = 1
-		adjacencyMatrix[v2][v1] = 1
-		adjacencyList[v1].append(v2)
-		adjacencyList[v2].append(v1)
-	print("The set of vertices of the graph is:")
-	print(vertices)
-	print("The set of edges of the graph is:")
-	print(edges)
-	print("The adjacency matrix representing the graph is:")
-	print(adjacencyMatrix)
-	print("The adjacency List representing the graph is:")
-	print(adjacencyList)
+# A class to represent a graph object
+class Graph:
+    # Constructor
+    def __init__(self, edges, N):
+ 
+        # A list of lists to represent an adjacency list
+        self.adjList = [[] for _ in range(N)]
+ 
+        # add edges to the undirected graph
+        for (src, dest) in edges:
+            self.adjList[src].append(dest)
+            self.adjList[dest].append(src)
