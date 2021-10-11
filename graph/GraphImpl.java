45e1d687c9d9ea93c37677e2f9d2aa98a604066d
joshuahenriques <joshuahenriques95@gmail.com>
2021-10-10 23:08:10 -0400

    Update GraphImpl.java

--- graph/GraphImpl.java
+++ graph/GraphImpl.java
@@ -3,30 +3,43 @@ import java.util.*;
 class GraphImpl<T> {
 
 	// edges
-	private Map<T, List<T>> map = new HashMap<>();
+	private Map<T, List<T>> adjVerticies = new HashMap<>();
 
 	// insert vertex
 	public void addVertex(T v) {
-		this.map.putIfAbsent(v, new LinkedList<T>());
+		this.adjVerticies.putIfAbsent(v, new LinkedList<T>());
 	}
 
-	public void addEdge(T src, T dest, boolean bi) {
-		if (!this.map.containsKey(src)) addVertex(src);
-		if (!this.map.containsKey(dest)) addVertex(dest);
-		this.map.get(src).add(dest);
-		if (bi) {
-			this.map.get(dest).add(src);
+	public void removeVertex(T v) {
+		this.adjVerticies.values().stream().forEach(e -> e.remove(v));
+		this.adjVerticies.remove(v);	
+	}
+
+	public void addEdge(T src, T dest) {
+		if (!this.adjVerticies.containsKey(src)) addVertex(src);
+		if (!this.adjVerticies.containsKey(dest)) addVertex(dest);
+		this.adjVerticies.get(src).add(dest);
+		this.adjVerticies.get(dest).add(src);
+	}
+
+	public void removeEdge(T src, T dest) {
+		List<T> srcList = this.adjVerticies.get(src);
+		List<T> destList = this.adjVerticies.get(dest);
+		if (srcList != null && destList != null) {
+			System.out.println("removeEdge");
+			this.adjVerticies.get(src).remove(dest);
+			this.adjVerticies.get(dest).remove(src);
 		}
 	}
 
 	public int getVertexCount() {
-		return this.map.keySet().size();
+		return this.adjVerticies.keySet().size();
 	}
 
 	public int getEdgesCount(boolean bi) {
 		int count = 0;
-		for (T v: map.keySet()) {
-			count += map.get(v).size();
+		for (T v: adjVerticies.keySet()) {
+			count += adjVerticies.get(v).size();
 		}
 		if (bi) {
 			count /= 2;
@@ -35,11 +48,11 @@ class GraphImpl<T> {
 	}
 
 	public boolean hasVertex(T v) {
-		return this.map.containsKey(v);
+		return this.adjVerticies.containsKey(v);
 	}
 
 	public boolean hasEdge(T s, T d) {
-		return this.map.get(s).contains(d);
+		return this.adjVerticies.get(s).contains(d);
 	}
 
 	public static void main(String args[]) {
@@ -50,30 +63,29 @@ class GraphImpl<T> {
         // edges are added.
         // Since the graph is bidirectional,
         // so boolean bidirectional is passed as true.
-        g.addEdge(0, 1, true);
-        g.addEdge(0, 4, true);
-        g.addEdge(1, 2, true);
-        g.addEdge(1, 3, true);
-        g.addEdge(1, 4, true);
-        g.addEdge(2, 3, true);
-        g.addEdge(3, 4, true);
+		g.addVertex(0);
+ 		g.addVertex(1);
+ 		g.addVertex(2);
+ 		g.addVertex(3);
+ 		g.addVertex(4);
+	 	g.addVertex(5);
+        g.addEdge(0, 1);
+        g.addEdge(0, 4);
+        g.addEdge(1, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 4);
+        g.addEdge(2, 3);
+        g.addEdge(3, 4);
   
-        // print the graph.
-        System.out.println("Graph:\n"
-                           + g.toString());
-  
-        // gives the no of vertices in the graph.
         System.out.println(g.getVertexCount());
   
-        // gives the no of edges in the graph.
-        System.out.println(g.getEdgesCount(true));
-  
-        // tells whether the edge is present or not.
-        System.out.println(g.hasEdge(3, 4));
-  
-        // tells whether vertex is present or notvoid
+        System.out.println(g.getEdgesCount(false));
         System.out.println(g.hasVertex(5));
-    }
+		g.removeVertex(5);
+        System.out.println(g.hasVertex(5));
+        System.out.println(g.getVertexCount()); 
+		System.out.println(g.hasEdge(0, 1));
+		g.removeEdge(0, 1);
+		System.out.println(g.hasEdge(0, 1));
+	}
 }
-
-  
