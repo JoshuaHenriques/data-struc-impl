99e99387920cda905c7e3d0263d20ddde318586d
joshuahenriques <joshuahenriques95@gmail.com>
2021-11-10 13:47:52 -0500

    create

--- graph/graph.py
+++ graph/graph.py
@@ -1,12 +1,24 @@
 # A class to represent a graph object
 class Graph:
     # Constructor
-    def __init__(self, edges, N):
+    def __init__(self, edges, n):
  
         # A list of lists to represent an adjacency list
-        self.adjList = [[] for _ in range(N)]
+        self.adjList = [[] for _ in range(n)]
  
-        # add edges to the undirected graph
+        # add edges to the directed graph
         for (src, dest) in edges:
             self.adjList[src].append(dest)
-            self.adjList[dest].append(src)
+        
+    # Function to print adjacency list representation of a graph
+    def printGraph(graph):
+        for src in range(len(graph.adjList)):
+            # print current vertex and all its neighboring vertices
+            for dest in graph.adjList[src]:
+                print(f'({src} —> {dest}) ', end='')
+            print()
+
+if __name__ == "__main__":
+    edges = [(0, 1), (1, 2), (2, 0), (2, 1), (3, 2), (4, 5), (5, 4)]
+    graph = Graph(edges, 6)
+    print(graph.adjList)
